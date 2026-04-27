34204117f0dec1b3abad8df93033d432828f3395
ahmedsaleh17 <ahmedsalehsh54@gmail.com>
2026-04-27 22:27:44 +0300

    Indexes on sql server

--- Learn-SQL/14_sql_indexes.sql
+++ Learn-SQL/14_sql_indexes.sql
@@ -1,6 +1,5 @@
 -- clustered & nonclustered indexes: 
-USE SalesDB;
-
+ 
 SELECT
     *
 FROM Sales.Customers
@@ -15,13 +14,16 @@ FROM SALES.Customers
 
 
 -- now search from the new table 
+-- Db customers table with no index 
+
 
 SELECT 
     *
 FROM Sales.DBCustomers
-WHERE CustomerID = 3 
+WHERE CustomerID = 1  
 -- To find the customer with id 3 the db engine full scans all table 
-
+-- HEAP STRUCTURE 
+-- EVEN THE ROW IN THE FIRST DATAPAGE THE DB ENGINE WILL DO ALSO FULL TABLE SCAN
 
 
 -- create new clustered index 
@@ -40,13 +42,23 @@ SELECT
     *
 FROM Sales.DBCustomers
 WHERE LastName = 'Brown'
-
--- To improve the performance of the previous query because you use alot
+-- If you frequently use this query and you want to improve the performance 
 -- you can create non clustered index
 
-
+-- Non-clustered index on lastname column 
 CREATE NONCLUSTERED INDEX idx_DBCustomers_LastName 
 ON Sales.DBCustomers(LastName)
+
+--
+SELECT
+    *
+FROM Sales.DBCustomers
+WHERE FirstName = 'Anna'
+-- If you frequently use this query and you want to improve the performance 
+-- you can create non clustered index
+
+CREATE INDEX Idx_DBCustomers_FirstName 
+ON Sales.DBCustomers(FirstName)
 
 
 -- composite Index
@@ -55,13 +67,15 @@ SELECT
 FROM SALES.DBCustomers
 WHERE Country = 'USA' AND Score > 500
 
--- WHAT IF YOU USE THE PREVIOUS QUERY ALOT 
+-- WHAT IF YOU frequently use  THE PREVIOUS QUERY  
 -- AND YOU NEED TO IMPROVE PERFORMANCE OF THE PREVIOUS QUERY
+
 CREATE INDEX idx_DBCustomers_CountryScore 
 ON Sales.DBCustomers(Country, Score) 
+
 -- Rule : the columns of index order must match the order in yyour query
 -- THE ORDER OF COLUMNS IN QUERY MUST BE THE SAME IN THE NON CLUSTERED INDEX
-
+-- The Order is very critical
 
 /* Leftmost Prefix Rule 
 index works only if your query filters start from 
@@ -127,62 +141,73 @@ RULE: the table have only one column store index either clustered or nonclustere
 -- let's make our table splitted by columns 
 -- but first we need to remove the existed clustered index 
 DROP INDEX idx_DBCustomers_CustomerID ON Sales.DBCustomers;
+
 CREATE CLUSTERED COLUMNSTORE INDEX Idx_DBCustomers ON SALES.DBCustomers
 
-
-
+/*
+Note:
+You can't create multiple column store index in the same table
+CREATE NONCLUSTERED COLUMNSTORE INDEX Idx_DBCustomers_FirstName 
+ON Sales.DBCustomers(FirstName)*/ 
 ----------------------------------------------------------
 
 -- creating 3 copies of table with different structure
 USE AdventureWorksDW2022;
 
+CREATE SCHEMA testing ; 
 
 -- heap structure 
 SELECT
     *
-INTO FactInternetSales_HP
+INTO testing.FactInternetSales_HP
 FROM FactInternetSales
 
 
 -- clustered rowstore index
 SELECT
     *
-INTO FactInternetSales_RS
+INTO testing.FactInternetSales_RS
 FROM FactInternetSales
 
 
 CREATE CLUSTERED INDEX IDX_FactInternetSales_RS_PK
-ON FactInternetSales_RS(SalesOrderNumber, SalesOrderLineNumber)
+ON testing.FactInternetSales_RS(SalesOrderNumber, SalesOrderLineNumber)
+
 
 -- clustered cloumnstore index
 SELECT
     *
-INTO FactInternetSales_CS
+INTO testing.FactInternetSales_CS
 FROM FactInternetSales
 
 
 CREATE CLUSTERED COLUMNSTORE INDEX IDX_FactInternetSales_CS_PK
-ON FactInternetSales_CS
+ON testing.FactInternetSales_CS
 
-
+/* If you check the storage of each table in testing schema you will notice that: 
+-- FactInternetSales_CS store has the lowest storage comparing to the 2 others table (Heap - RowStore)
+HEAP = 9.641 MP
+ROWSTORE CLUSTERED = 9.688 MP INCREASED BECAUSE THE INDEX STORAGE ADDED 
+COLUMNSTORE CLUSTERED = 1.461 MP 
+*/ 
 
 -- Display Estimated Execution Plan
 
 
 SELECT -- TABLE FULL SCAN
     *
-FROM FactInternetSales_HP
+FROM testing.FactInternetSales_HP
 
 
 
 SELECT    -- CLUSTERED INDEX SCAN
 	*
-FROM FactInternetSales_RS
+FROM testing.FactInternetSales_RS
 
 
 SELECT   -- COLUMNSTORE INDEX SCAN
 	*
-FROM FactInternetSales_CS
+FROM testing.FactInternetSales_CS
 
 
 -----------------------------------------
